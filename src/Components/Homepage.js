a847b4d24cf8c28bf01380944b0a45dbe4b48055
saimsaleem <engssaleem@gmail.com>
2023-09-14 13:34:32 +0500

    ImportModel

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -5,7 +5,6 @@ import './Homepage.css'
 export default function Homepage(){
     return(
         <>
-        <Navbar/>
         <h1 className='welcome'>Welcome</h1>
         <a href=""><h1>Hello</h1></a>
         </>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,14 +5,12 @@ export default function(){
     return(
     <>
         <nav className='navbarr'>
-            <div>
-                <a className='nav1' href="">Home</a>
+            <div className='flex'>
+              <div>  <a className='nav1' href="">Home</a>
                 <a className='nav1' href="">Features</a>
                 <a className='nav1' href="">How to use</a>
-                <a className='nav1' href="/importmodel">Import Model</a>
-            </div>
-            <div className='nav2'>
-                <a className='nav1 right' href="">Login</a>
+                <a className='nav1' href="/importmodel">Import Model</a> </div>
+                <div className='right'><a className='nav1' href="">Login</a></div>
             </div>
         </nav>
     </>
