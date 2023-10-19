1bc7d0c3273e26c9b276518ee05a69388cf37be2
Sadia <sadobatool>
2023-10-19 12:21:57 +0500

    Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import ModelView from './Components/ModelView';
 import Homepage from './Components/Homepage'
 import Maps from './Components/Maps'
+import {Routes, BrowserRouter, Route} from 'react-router-dom'
+import FeatureSelection from './Components/FeatureSelection'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage/> */}
-      <div className='modelview'>
-    
-      </div>
-      <Maps/>
+      {/* <FeatureSelection/> */}
+      <BrowserRouter>
+    <Routes>
+      {/* <Route path="/home" element={<Homepage/>}/>  */}
+      <Route path="/" element={<FeatureSelection/>} />
+      <Route path="/map" element={<Maps/>} />
+      <Route path="/model" element={<ModelView/>} />
+    </Routes>
+  </BrowserRouter>
     </div>
   );
 }
