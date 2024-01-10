b4d8b2c912fe7c1d67fc37f34cd532955783e84a
saimsaleem <engssaleem@gmail.com>
2024-01-11 00:16:03 +0500

    Landing Page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Footer.css"
+export default function Footer() {
+  return (
+    <div>
+      <div className="background">
+        <div className="logos">
+            <div className="logo">
+                <img src="/Icons/FYPLogo.png" alt="" />
+            </div>
+            <div className="icons">
+                <img src="/Icons/Instagram.png" alt="" />
+                <img src="/Icons/Facebook.png" alt="" />
+            </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "./Navbar";
-
+import Footer from './Footer'
 export const Homepage = () => {
   return (
     <>
@@ -65,6 +65,7 @@ export const Homepage = () => {
           </div>
       </div>
     </div>
+    <Footer/>
   </>
     );
 };
