655acfa980fd811dc2d53c843b76bad6d4669053
saimsaleem <engssaleem@gmail.com>
2024-01-17 18:53:01 +0500

    Homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Sidebar } from "../Layouts/Sidebar";
+
 export const Home = () => {
   return (
     <>
@@ -13,11 +14,46 @@ export const Home = () => {
     <div className="home">
 
       <div className="main">
+
         <div className="box">
          <div className="heading"><h1>Design Your Home</h1></div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, tempora nihil esse dolor ad placeat inventore ducimus. Soluta, unde optio mollitia aperiam magnam facilis, laboriosam repellat, blanditiis eaque officia enim.</p>
+         <div className="subheading"><h2>Choose Operation</h2></div>
+         <div className="links">
+          <div className="buttons">
+
+            <div className="button">
+              <button id="foryou"><i class="fa fa-home" aria-hidden="true"></i></button>
+              <p>For You</p>
+            </div>
+
+            <div className="button">
+              <button><i class="fa fa-map" aria-hidden="true"></i></button>
+              <p>Map Digitization</p>
+            </div>
+
+            <div className="button">
+              <button><i class="fa fa-cubes" aria-hidden="true"></i></button>
+              <p>Model Generation</p>
+            </div>
+
+            <div className="button">
+              <button><img src="/Icons/room-icon.webp" height={40} width={40}/></button>
+              <p>Space Generation</p>
+            </div>
+
+            <div className="button">
+              <button><i class="fa fa-building" aria-hidden="true"></i></button>
+              <p>Front Elevation</p>
+            </div>
+
+          </div>
+         </div>
         </div>
+
+
       </div>
+
+
     </div>
     
     </div>
